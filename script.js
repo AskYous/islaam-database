40c3fa3f1ed1ef17fcfd066c2b1601843a11850e
Yousef Shanawany <askyous@gmail.com>
2016-09-05 17:58:48 -0700

    Got people and relations

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var key = 'AIzaSyDQNt2FlhWtrMUyNfS0pwXA9ngh904XrNk';
 var sheetId = '1oEhVbC85KnVYpjOnqX18plTSyjyH6F4dxNQ4SjjkBAs';
-var sheetUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + sheetId + "?key=" + key;
-$.getJSON(sheetUrl, function (results) { return console.log(results); }, function (error) { return console.error(error); });
+var range = 'ranges=Humans!A:I&ranges=Students!A:F';
+var sheetUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + sheetId + "/values:batchGet?ranges=Humans!A:I&ranges=Students!A:F&key=" + key;
+$.getJSON(sheetUrl, function (results) {
+    var people = results.valueRanges[0].values;
+    var relations = results.valueRanges[1].values;
+    console.log(results);
+    console.log(people);
+    console.log(relations);
+}, function (error) { return console.error(error); });
--- script.ts
+++ script.ts
@@ -1,5 +1,14 @@
 const key = 'AIzaSyDQNt2FlhWtrMUyNfS0pwXA9ngh904XrNk';
 const sheetId = '1oEhVbC85KnVYpjOnqX18plTSyjyH6F4dxNQ4SjjkBAs';
-const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}?key=${key}`;
+const range = 'ranges=Humans!A:I&ranges=Students!A:F';
+const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values:batchGet?ranges=Humans!A:I&ranges=Students!A:F&key=${key}`;
 
-$.getJSON(sheetUrl, results => console.log(results), error=> console.error(error));
+$.getJSON(sheetUrl, results => {
+  const people = results.valueRanges[0].values;
+  const relations = results.valueRanges[1].values;
+
+  console.log(results);
+  console.log(people);
+  console.log(relations);
+
+}, error=> console.error(error));
