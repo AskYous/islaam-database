c7dd9de93c17f20b5514c561dd76178cb3c5a02d
Yousef Shanawany <askyous@gmail.com>
2016-09-06 21:21:05 -0700

    Using live code from svg-ontology-graph's github AND from google sheets!

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var svg = document.querySelector('svg');
 var key = 'AIzaSyDQNt2FlhWtrMUyNfS0pwXA9ngh904XrNk';
 var sheetId = '1oEhVbC85KnVYpjOnqX18plTSyjyH6F4dxNQ4SjjkBAs';
 var range = 'ranges=Humans!A:I&ranges=Students!A:F';
@@ -5,7 +6,22 @@ var sheetUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + sheetId + "/va
 $.getJSON(sheetUrl, function (results) {
     var people = results.valueRanges[0].values;
     var relations = results.valueRanges[1].values;
+    people = people.slice(1, people.length - 1);
+    relations = relations.slice(1, relations.length - 1);
+    people = people.map(function (person) {
+        var id = parseInt(person[0]);
+        var name = person[1];
+        return {
+            'id': id,
+            'name': name
+        };
+    });
+    relations = relations.map(function (relation) {
+        return [parseInt(relation[0]), parseInt(relation[1])];
+    });
     console.log(results);
     console.log(people);
     console.log(relations);
+    debugger;
+    var graph = new SVGOntologyGraph(people, relations, svg);
 }, function (error) { return console.error(error); });
--- script.ts
+++ script.ts
@@ -1,14 +1,37 @@
+const svg = document.querySelector('svg');
+
 const key = 'AIzaSyDQNt2FlhWtrMUyNfS0pwXA9ngh904XrNk';
 const sheetId = '1oEhVbC85KnVYpjOnqX18plTSyjyH6F4dxNQ4SjjkBAs';
 const range = 'ranges=Humans!A:I&ranges=Students!A:F';
 const sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values:batchGet?ranges=Humans!A:I&ranges=Students!A:F&key=${key}`;
 
 $.getJSON(sheetUrl, results => {
-  const people = results.valueRanges[0].values;
-  const relations = results.valueRanges[1].values;
+  let people = <any[]> results.valueRanges[0].values;
+  let relations = <any[]> results.valueRanges[1].values;
+
+  // Skip first elements
+  people = people.slice(1, people.length - 1);
+  relations = relations.slice(1, relations.length - 1);
+
+  // convert to digraph-friendly objects
+  people = people.map(person => {
+    const id = parseInt(person[0]);
+    const name = person[1];
+
+    return {
+      'id': id,
+      'name': name
+    }
+  });
+
+  relations = relations.map(relation => {
+    return [parseInt(relation[0]), parseInt(relation[1])];
+  })
 
   console.log(results);
   console.log(people);
   console.log(relations);
+debugger;
+  const graph = new SVGOntologyGraph(people, relations, svg);
 
 }, error=> console.error(error));
